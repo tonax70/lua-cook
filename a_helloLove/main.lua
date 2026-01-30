b0b2a63af49abd662ca431aa6722b08d4303f01f
tonax70 <canhtoan9a@gmail.com>
2026-01-30 11:51:49 +0700

    basic if

--- a_helloLove/main.lua
+++ a_helloLove/main.lua
@@ -1,13 +1,19 @@
 message = "I cook lua\n"; -- string
 number = 67; -- number
-local currentDateTime = os.date()
+local currentDateTime = os.date();
+response = "";
+
+if (number == 67) then 
+    response="uh uh whish i wish i knew knew knew knew";
+end
 
 function love.draw()
-    love.graphics.setFont(love.graphics.newFont(30))
+    love.graphics.setFont(love.graphics.newFont(30));
     -- love.graphics.print(text, x, y)
-    love.graphics.print("Hello", 10, 10) 
-    love.graphics.setFont(love.graphics.newFont(20))
-    love.graphics.print("lua variables:", 10, 70) 
-    love.graphics.print("string: " .. message .. "\n" .. "number: " .. number, 10, 120)
-    love.graphics.print("date: " ..currentDateTime, 10, 143)
+    love.graphics.print("Hello", 10, 10) ;
+    love.graphics.setFont(love.graphics.newFont(20));
+    love.graphics.print("lua variables:", 10, 70) ;
+    love.graphics.print("string: " .. message .. "\n" .. "number: " .. number, 10, 120);
+    love.graphics.print("date: " ..currentDateTime, 10, 143);
+    love.graphics.print("date: " ..response, 10, 190);
 end
