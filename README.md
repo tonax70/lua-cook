b5ae276d21e045a2847c026657880042ccd5ceb3
tonax70 <canhtoan9a@gmail.com>
2026-01-30 11:34:55 +0700

    7 - variables

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # I cook lua to see if it helps anything
 ## > [if using vscodium: modifying vscodium extension repo](./VSCODIUM_SETUP.md)
 
-- jump start - try love2d for fun - jan 30 2026
+1. jump start - try love2d for fun - jan 30 2026
+    - love is code focused paradigm, you should build everything from ground up with code
+    - install love
+    - install vscode extensions: love support
+    - alt + l to run project
 
 
--- a_helloLove/main.lua
+++ a_helloLove/main.lua
@@ -1,4 +1,11 @@
+message = "I cook lua\n"; -- string
+number = 67; -- number
+
 function love.draw()
-    love.graphics.setFont(love.graphics.newFont(50));
-    love.graphics.print( "Hello");
+    love.graphics.setFont(love.graphics.newFont(30))
+    -- love.graphics.print(text, x, y)
+    love.graphics.print("Hello", 10, 10) 
+    love.graphics.setFont(love.graphics.newFont(20))
+    love.graphics.print("lua variables:", 10, 70) 
+    love.graphics.print("string: " .. message .. "\n" .. "number: " .. number, 10, 120)
 end
