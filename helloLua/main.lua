078c6e6a22c0fb8c8a0ac6d5076c1126573d00da
tonax70 <canhtoan9a@gmail.com>
2026-01-30 14:01:06 +0700

    lua table

--- helloLua/main.lua
+++ helloLua/main.lua
@@ -28,9 +28,28 @@ for i=1, 67, 1 do
     sum = sum + i;
 end
 
+-- functions
 local function lp (message, x, y)
     love.graphics.print(message, x, y);
 end
+
+
+-- array/object ~ TABLE
+
+local arr = {};
+
+-- add value to a key method 1
+arr[1]  = "SKS";
+arr[2] = "NKN";
+arr[3] = "SRO";
+
+-- insert method 2
+table.insert(arr, "KJI"); -- it will assign with a number
+table.insert(arr, "SRA");
+
+-- can understand table as a key - value
+arr["SKSST"] = "goated";
+arr[arr[1]] = 800; -- wut da is real
 
 function love.draw()
     love.graphics.setFont(love.graphics.newFont(30));
@@ -42,4 +61,7 @@ function love.draw()
     love.graphics.print("date: " ..currentDateTime, 10, 143);
     lp("date: " ..response, 10, 190);
     lp("the sum from 1 to 67 is: "..sum, 10, 220);
+    lp("array test: ".. arr[4], 10, 250);
+    lp("array test: ".. arr["SKS"], 10, 280);
+    lp("array test: ".. arr["SKSST"], 10, 300);
 end
