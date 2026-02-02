2809471c7ead437dd177c35f498aa74d1012a299
tonax70 <canhtoan9a@gmail.com>
2026-02-02 17:26:12 +0700

    doc menu updated

--- docPrinter/print.f90
+++ docPrinter/print.f90
@@ -13,6 +13,18 @@ program doc
     case ('2')
     call docPrint("Basic love funcions: load, update, draw", "./basicLove/main.lua");
 
+    case ('3')
+    call docPrint("Basic shapes and color", "./basicShapesAndColor/main.lua");
+
+    case ('4')
+    call docPrint("legacy bouncing ball logic", "./basicBouncingBall/logic.ts");
+    call docPrint("legacy bouncing ball code", "./basicBouncingBall/main.lua");
+
+    case ('5')
+    call docPrint("new bouncing ball logic", "./basicBouncingBall/logicImproved.ts");
+    call docPrint("new bouncing ball code", "./basicImprovedBall/main.lua");
+
+    
     case default 
     print *, "invalid case, exit.";
     end select
