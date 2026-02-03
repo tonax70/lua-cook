222c65d6317f159d21586cb758f4bd2a06ae901a
tonax70 <canhtoan9a@gmail.com>
2026-02-03 10:54:03 +0700

    bash formatter fixed

--- doc.sh
+++ doc.sh
@@ -1,46 +1,45 @@
 #!/usr/bin/bash
 
-OPTION="y";
+OPTION="y"
 
+FORMATTER_FIX () {
+        cat "./bashFormatterFix.txt"
+}
 MENU() {
-echo -e "Choose which to view: \n";
-cat "./docPrinter/menu.txt";
+    echo -e "Choose which to view: \n"
+    cat "./docPrinter/menu.txt"
 }
 
 EXECUTE_PROG() {
-sh -c "gfortran ./docPrinter/utils.f90 ./docPrinter/print.f90 -o ./docPrinter/print && ./docPrinter/print";
+    sh -c "gfortran ./docPrinter/utils.f90 ./docPrinter/print.f90 -o ./docPrinter/print && ./docPrinter/print"
 }
 
 EXECUTE_LOOP() {
-    for((;;))
-    do 
-        echo "Do you want to continue? (y/n): ";
-        read OPTION;
+    for (( ; ; )); do
+        echo "Do you want to continue? (y/n): "
+        read OPTION
 
-        if  [[ $OPTION == 'y' || $OPTION == 'Y' ]]
-        then
-            EXECUTE_PROG;
-        else 
-            echo "Exit.";
-            break;
+        if [[ $OPTION == 'y' || $OPTION == 'Y' ]]; then
+            EXECUTE_PROG
+        else
+            echo "Exit."
+            break
         fi
-    done 
+    done
 }
-if [[ $1 == "verbose" ]]
-then
-if [[ $1 == "verbose" && $2 == "full" ]]
-then
-echo "Welcome to tonax's code viewer and inspector!";
-MENU;
-EXECUTE_PROG;
-EXECUTE_LOOP;
+if [[ $1 == "verbose" ]]; then
+    if [[ $1 == "verbose" && $2 == "full" ]]; then
+        echo "Welcome to tonax's code viewer and inspector!"
+        FORMATTER_FIX
+        MENU
+        EXECUTE_PROG
+        EXECUTE_LOOP
+    else
+        echo "Welcome to tonax's code viewer and inspector!"
+        MENU
+        EXECUTE_PROG
+        EXECUTE_LOOP
+    fi
 else
-echo "Welcome to tonax's code viewer and inspector!";
-EXECUTE_PROG;
-EXECUTE_LOOP;
+    EXECUTE_PROG
 fi
-else 
-EXECUTE_PROG;
-fi
-
-
